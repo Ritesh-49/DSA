0e98aae939eef483b965f5014a698424ee8177e9
Ritesh kumar <riteshk2323@gmail.com>
2026-08-16 23:28:35 +0530

    Patterns

---
+++ Patterns/src/numberPyramidPalindrome.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class numberPyramidPalindrome {
+    Scanner sc = new Scanner(System.in);
+
+    public void Pyramid(){
+        System.out.println("Enter n: ");
+        int n= sc.nextInt();
+
+        for (int i = 1; i <=n ; i++) {
+
+            for (int j = 1; j <=n-i ; j++) {
+                System.out.print(" " + " ");
+            }
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(j + " ");
+            }
+
+            for (int j = i-1; j >=1; j--) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+
+    }
+
+    public  void starBridge(){
+        System.out.println("Enter n: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i<=2*n-1 ; i++) {
+            System.out.print("*" + " ");
+        }
+        System.out.println();
+
+        n--;
+
+        for (int i = 1; i <=n ; i++) {
+
+            for (int j = 1; j <=n+1-i ; j++) {
+                System.out.print("*" + " ");
+            }
+            for (int j = 1; j <=2*i-1 ; j++) {
+                System.out.print(" " + " ");
+            }
+
+            for (int j = 1; j <=n+1-i; j++) {
+                System.out.print("*" + " ");
+            }
+
+            System.out.println();
+        }
+
+
+    }
+
+    public  static void main(String[] args) {
+        numberPyramidPalindrome n = new numberPyramidPalindrome();
+        n.starBridge();
+    }
+}
