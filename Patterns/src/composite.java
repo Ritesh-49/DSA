f85d9534fba793645f6ef39c2216b211a6ff4e81
Ritesh kumar <riteshk2323@gmail.com>
2026-08-16 17:20:17 +0530

    updated composite pattern

--- Patterns/src/composite.java
+++ Patterns/src/composite.java
@@ -3,7 +3,58 @@ import java.util.Scanner;
 public class composite {
     Scanner sc = new Scanner(System.in);
 
+    public void NumberTriangleVerticalFliped(){
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n ; i++) {
+            for (int j = 1; j <= n-i ; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(j + " ");
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public void rhombus(){
+        System.out.println("Enter n: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=n-i ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <=n ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+
+        }
+    }
+
+    public void piramid(){
+        System.out.print("Enter n:");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=n-i ; j++) {
+                System.out.print(" " + " ");
+            }
+            for (int j = 1; j <=2*i-1 ; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+    }
     public static void main(String [] args){
         System.out.println("Ritesh");
+
+        composite c = new composite();
+        c.piramid();
     }
 }
