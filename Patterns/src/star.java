6884957c46bd2cba9553479e0765b7b3741edba0
Ritesh kumar <riteshk2323@gmail.com>
2026-08-16 17:23:17 +0530

    star patterns

--- Patterns/src/star.java
+++ Patterns/src/star.java
@@ -1,4 +1,51 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class star {
+    Scanner sc = new Scanner(System.in);
+
+    public void starPlus(){
+        System.out.println("Enter rows: ");
+        int n = sc.nextInt();
+        int mid = n/2 + 1;
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <=n ; j++) {
+                if (j==mid || i==mid){
+                    System.out.print("*" + " ");
+                }
+                else {
+                    System.out.print(" " + " ");
+                }
+
+            }
+            System.out.println();
+
+        }
+    }
+
+    public void starCross(){
+        System.out.print("Enter rows: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <=n ; j++) {
+//                if (i==j || j==n+1-i){
+//                    System.out.print("*");
+//                }
+
+                if (i==j || i+j == n + 1){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" "+ " ");
+                }
+
+
+            }
+            System.out.println();
+
+        }
+    }
+    public static void main(String[] args){
+        star s = new star();
+        s.starCross();
+    }
 }
