5fdd8acbedf9e7500634d3a10980207b5ade57ce
Ritesh kumar <riteshk2323@gmail.com>
2026-08-18 23:58:33 +0530

    star dimond

--- Patterns/src/composite.java
+++ Patterns/src/composite.java
@@ -51,10 +51,41 @@ public class composite {
         }
 
     }
+
+    public void StarDimond(){
+        System.out.println("Enter n: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=n-i ; j++) {
+                System.out.print(" " + " ");
+            }
+            for (int j = 1; j <=2*i-1 ; j++) {
+                System.out.print("*" + " ");
+            }
+
+
+            System.out.println();
+        }
+
+        for (int i = 1; i < n; i++) {
+
+            // Spaces
+            for (int j = 1; j <= i; j++) {
+                System.out.print("  ");
+            }
+
+            // Stars
+            for (int j = 1; j <= 2 * (n - i) - 1; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
     public static void main(String [] args){
         System.out.println("Ritesh");
 
         composite c = new composite();
-        c.piramid();
+        c.StarDimond();
     }
 }
