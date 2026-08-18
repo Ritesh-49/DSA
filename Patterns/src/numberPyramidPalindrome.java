107ecbb18ca6b5251ed8235d9da756fb708695b4
Ritesh kumar <riteshk2323@gmail.com>
2026-08-18 23:29:20 +0530

    Number Bridge Pyramid

--- Patterns/src/numberPyramidPalindrome.java
+++ Patterns/src/numberPyramidPalindrome.java
@@ -55,8 +55,40 @@ public class numberPyramidPalindrome {
 
     }
 
+    public  void numberBridge(){
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i<=2*n-1 ; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        n--;
+
+        for (int i = 1; i <=n ; i++) {
+            int a=1;
+
+            for (int j = 1; j <=n+1-i ; j++) {
+                System.out.print(a++ + " ");
+            }
+            for (int j = 1; j <=2*i-1 ; j++) {
+                System.out.print(" " + " ");
+                a++;
+            }
+
+            for (int j = 1; j <=n+1-i; j++) {
+                System.out.print(a++ + " ");
+            }
+
+            System.out.println();
+        }
+
+
+    }
+
     public  static void main(String[] args) {
         numberPyramidPalindrome n = new numberPyramidPalindrome();
-        n.starBridge();
+        n.numberBridge();
     }
 }
