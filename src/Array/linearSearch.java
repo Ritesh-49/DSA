018b33538895afd7e752dc70ed35439e38470f3e
Ritesh kumar <riteshk2323@gmail.com>
2026-08-20 23:48:40 +0530

    LenearSearch

---
+++ src/Array/linearSearch.java
@@ -0,0 +1,43 @@
+package Array;
+
+import java.util.Scanner;
+
+public class linearSearch {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter size of array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter array element: ");
+        for (int i = 0; i < n; i++) {
+            arr[i]= sc.nextInt();
+        }
+
+        System.out.println("Enter Element to search: ");
+        int s = sc.nextInt();
+
+
+        boolean flag = false;
+        for (int i = 0; i < n; i++) {
+            if( s == arr[i]){
+                flag = true;
+//                System.out.println(" the valu is matched: "+ arr[i]);
+                break;
+            }
+
+        }
+        if (flag== true){
+            System.out.println("Found");
+        }
+        else {
+
+            System.out.println("no");
+        }
+
+
+
+    }
+}
