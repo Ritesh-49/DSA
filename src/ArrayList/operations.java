f4d16ed6cf29a1d342ae81f2d95cd89c9623554b
Ritesh kumar <riteshk2323@gmail.com>
2026-08-23 00:19:22 +0530

    Basic operation on arraylsit

--- src/ArrayList/operations.java
+++ src/ArrayList/operations.java
@@ -1,4 +1,28 @@
 package ArrayList;
 
+import java.util.ArrayList;
+
 public class operations {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        System.out.println(list + " " +  list.size());  //default size of the array lsit is 0,
+
+        list.add(0, 20);
+        System.out.println(list + " " +  list.size());
+
+        list.add(1, 30);
+        System.out.println(list + " " +  list.size());
+
+        list.add(2, 30);
+        System.out.println(list + " " +  list.size());
+
+        list.add(3, 40);
+        System.out.println(list + " " +  list.size());
+
+        list.remove(1);
+        System.out.println(list + " " +  list.size());
+
+    }
 }
