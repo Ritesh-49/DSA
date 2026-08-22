7816237163482e95b8d62a80c7ae4aece15e5495
Ritesh kumar <riteshk2323@gmail.com>
2026-08-22 23:18:07 +0530

    Array methods

--- src/Array/arrayMethods.java
+++ src/Array/arrayMethods.java
@@ -1,12 +1,28 @@
 package Array;
 
+import java.util.Arrays;
+
 public class arrayMethods {
 
     public static void main(String[] args) {
-        int[] arr = {3,5,6,6,7,8,8,9};
+        int[] arr = {10,5,6,6,7,8,8,9};
 
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]);
+            System.out.print(arr[i] + " ");
         }
+
+        for (int x : arr){  // forEach loop
+            System.out.print(x);
+        }
+
+        Arrays.sort(arr);
+        System.out.println();
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+
+
     }
 }
