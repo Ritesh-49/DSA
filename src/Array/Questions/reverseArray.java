8933b21223c003b848412583268a5145b7885ec8
Ritesh kumar <riteshk2323@gmail.com>
2026-08-25 00:03:30 +0530

    Two pointer

--- src/Array/Questions/reverseArray.java
+++ src/Array/Questions/reverseArray.java
@@ -21,10 +21,63 @@ public class reverseArray {
             System.out.print(elem + " ");
         }
     }
+
+    void reverseUsingTwoPointer(){
+        int [] arr = {2,4,6,7,9,10,1};
+        for (int elem : arr){
+            System.out.print(elem + " ");
+        }
+
+        System.out.println();
+
+
+        int i = 0;
+        int j = arr.length - 1;
+
+        while(i<j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+
+        for (int elem : arr){
+            System.out.print(elem + " ");
+        }
+    }
+
+    void reverseUsingTwoPointerUsingFunction(){
+        int [] arr = {2,4,6,7,9,10,1};
+        for (int elem : arr){
+            System.out.print(elem + " ");
+        }
+
+        System.out.println();
+        int i = 0;
+        int j = arr.length - 1;
+
+        while(i<j){
+            swap(arr, i, j); // function
+
+            i++;
+            j--;
+        }
+
+        for (int elem : arr){
+            System.out.print(elem + " ");
+        }
+    }
+
+    public static  void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
     public static void main(String[] args) {
 
         reverseArray r = new reverseArray();
-        r.reverse1();
+        r.reverseUsingTwoPointerUsingFunction();
 
     }
 }
