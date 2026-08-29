a62a8045cb0bb58706ccde85f7245bfdf59489ad
Ritesh kumar <riteshk2323@gmail.com>
2026-08-30 00:01:50 +0530

    sort the arrays in 0's and 1's

--- src/Array/Questions/sortOnesAndZeros.java
+++ src/Array/Questions/sortOnesAndZeros.java
@@ -1,4 +1,29 @@
 package Array.Questions;
 
 public class sortOnesAndZeros {
+
+    void method1(int [] arr, int noOfZeros){
+        int n = arr.length;
+
+        for (int i = 0; i <n ; i++) {
+            if (arr[i] == 0 )
+                noOfZeros++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (i<noOfZeros)
+                arr[i] = 0;
+            else arr[i] =1;
+        }
+
+
+    }
+
+    static void main(String[] args) {
+        int [] arr = {0,1,0,1,1,0,0,1,0,1,0};
+        int noOfZeros = 0;
+
+        sortOnesAndZeros s = new sortOnesAndZeros();
+        s.method1(arr, noOfZeros);
+    }
 }
