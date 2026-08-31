a4bf1e25875e6c546d55a09b4267a973acb64a25
Ritesh kumar <riteshk2323@gmail.com>
2026-08-31 23:23:02 +0530

    updates

--- src/ArrayList/basics.java
+++ src/ArrayList/basics.java
@@ -36,6 +36,10 @@ public class basics {
 
 
         // Impotant
+        // The difference between a built-in array and an ArrayList in Java,
+        // is that the size of an array cannot be modified (if you want to add or remove
+        // elements to/from an array, you have to create a new one). While elements can be
+        // added and removed from an ArrayList whenever you want.
 
         /* arr.add(0, 20) // initializa the value at 0 index
         arr.set1(0,300) // it will modify/ update the value
