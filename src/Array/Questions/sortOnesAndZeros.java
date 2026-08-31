945433d25338ce8d30a8dbd69d4e59a2d3fe23c3
Ritesh kumar <riteshk2323@gmail.com>
2026-08-31 23:13:58 +0530

    sort the arrays in 0's and 1's m2

--- src/Array/Questions/sortOnesAndZeros.java
+++ src/Array/Questions/sortOnesAndZeros.java
@@ -2,8 +2,8 @@ package Array.Questions;
 
 public class sortOnesAndZeros {
 
-    void method1(int [] arr, int noOfZeros){
-        int n = arr.length;
+    void method1(int [] arr, int noOfZeros){    // array = {1,0,1,1,0,1,0,0,1,0}
+        int n = arr.length;                     // sort = {0,0,0,0,0,1,1,1,1,1}
 
         for (int i = 0; i <n ; i++) {
             if (arr[i] == 0 )
@@ -16,14 +16,74 @@ public class sortOnesAndZeros {
             else arr[i] =1;
         }
 
+        for(int elem : arr){
+            System.out.print(elem + " ");
+        }
 
+        System.out.println();
+    }
+
+    void method2(int [] arr){
+        int n = arr.length;
+        int i = 0 , j = n-1;
+
+       /* while(i<j){
+            if (arr[i] == 0) i++;
+            if (arr[j] == 1) j--;
+            if (i>j) break;
+            if (arr[i] == 1 && arr[j] == 0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }*/
+
+        while(i<j){
+            if (arr[i] == 0) i++;
+            if (arr[j] == 1) j--;
+//            if (i>j) break;
+            if (i<j && arr[i] == 1 && arr[j] == 0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+       /* while(i<j){
+            if (arr[i] == 0){
+                i++;
+            }
+            else if (arr[j] == 1){
+                j--;
+            }
+//            if (i>j) break;
+            else if (arr[i] == 1 && arr[j] == 0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        } */
+
+
+
+        for(int elem : arr){
+            System.out.print(elem + " ");
+        }
     }
 
     static void main(String[] args) {
-        int [] arr = {0,1,0,1,1,0,0,1,0,1,0};
+        int [] arr = {1,0,0,1,1,0,0,1,0,1,0};
         int noOfZeros = 0;
 
         sortOnesAndZeros s = new sortOnesAndZeros();
-        s.method1(arr, noOfZeros);
+//        s.method1(arr, noOfZeros);
+
+        int [] arr1 = {0,0,0,1,1,1};
+        s.method2(arr1);
     }
 }
