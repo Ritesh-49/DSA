59d0de9daeeccbb9257f28df6d259cc71ed086c6
Ritesh kumar <riteshk2323@gmail.com>
2026-09-06 17:22:52 +0530

    find min in 2D

--- src/TwoDArray/Questions/maxinArray.java
+++ src/TwoDArray/Questions/maxinArray.java
@@ -3,23 +3,35 @@ package TwoDArray.Questions;
 public class maxinArray {
     public static void main(String[] args) {
 //        int [][] arr = new int[][];
-        int [][] arr = {{2,3,4,}, {3,5,8}, {9,2,3},{3,6,7}} ;
+        int [][] arr = {{2,3,4,}, {3,5,8}, {9,2,3},{3,6,6}} ;
 
 //        int mx = arr[0][0];
         int mx = Integer.MIN_VALUE;
 
 
+//        for (int i = 0; i < 4; i++) {
+//            for (int j = 0; j < 3; j++) {
+////                if (arr[i][j] > mx){
+////                    mx = arr[i][j];
+////                }
+//
+//                mx = Math.max(mx, arr[i][j]);
+//            }
+//        }
+
+        int min = Integer.MAX_VALUE;
+
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 3; j++) {
-//                if (arr[i][j] > max){
-//                    max = arr[i][j];
+//                if (arr[i][j] < min){
+//                    min = arr[i][j];
 //                }
 
-                mx = Math.max(mx, arr[i][j]);
+                min = Math.min(min, arr[i][j]);
             }
         }
 
-        System.out.println(mx);
+        System.out.println(min);
 
     }
 }
