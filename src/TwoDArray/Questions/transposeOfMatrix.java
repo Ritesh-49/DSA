af863d957163d68aa4cb6d0660bc4d8230e31297
Ritesh kumar <riteshk2323@gmail.com>
2026-09-07 23:30:37 +0530

    Tranaspose of Matrix

--- src/TwoDArray/Questions/transposeOfMatrix.java
+++ src/TwoDArray/Questions/transposeOfMatrix.java
@@ -4,7 +4,7 @@ public class transposeOfMatrix {
     public static void main(String[] args) {
 
 
-        int [][] a = {{1,2},{3,4},{5,6}};
+        int [][] a = {{1,2,3},{4,5,6},{7,8,9}};
 
         int m = a.length;
         int n = a[0].length;
@@ -16,14 +16,37 @@ public class transposeOfMatrix {
             System.out.println();
         }*/
 
+         for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
         // transpose and store it in another array
         int [][] transpose = new int[n][m];
         for (int j = 0; j < n; j++) {
             for (int i = 0; i < m; i++) {
 //                System.out.print(a[i][j] + " ");
-                transpose[j][i] = a[i][j];
-                System.out.print(transpose[j][i] + " ");
+//                transpose[j][i] = a[i][j];
+//                System.out.print(transpose[j][i] + " ");
+
             }
+            System.out.println();
+        }
+
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j <= i; j++) {
+                int temp = a[i][j];
+                a[i][j] = a[j][i];
+                a[j][i] = temp;
+
+
+                System.out.print(a[i][j] + " ");
+            }
+
             System.out.println();
         }
 
