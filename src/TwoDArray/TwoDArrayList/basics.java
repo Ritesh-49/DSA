3308b2116a3a8ad886ecdacf8fb75b286ca31cb1
Ritesh kumar <riteshk2323@gmail.com>
2026-09-14 23:55:39 +0530

    Two D ArrayList

--- src/TwoDArray/TwoDArrayList/basics.java
+++ src/TwoDArray/TwoDArrayList/basics.java
@@ -1,4 +1,45 @@
 package TwoDArray.TwoDArrayList;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class basics {
+    public static void main(String[] args) {
+        List<Integer> a = new ArrayList<>();
+        a.add(10); a.add(20); a.add(30);
+        List<Integer> b = new ArrayList<>();
+        b.add(40); b.add(50);
+        List<Integer> c  = new ArrayList<>();
+        List<Integer> d  = new ArrayList<>();
+       d.add(60);
+       List<List<Integer>> l = new ArrayList<>();
+       l.add(a); l.add(b); l.add(c); l.add(d);
+
+        System.out.println();
+
+        for (int i = 0; i < l.size(); i++) {
+//            System.out.println(l.get(i));
+
+            List<Integer> x = l.get(i);
+            for (int j = 0; j <x.size() ; j++) {
+                System.out.print(x.get(j));
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < l.size(); i++) {
+//            System.out.println(l.get(i));
+
+            for (int j = 0; j <l.get(i).size() ; j++) {
+                System.out.print(l.get(i).get(j) + " ");
+            }
+            System.out.println();
+        }
+
+//        System.out.println(l.get(0).get(1));
+
+//        for (int i = 0; i < a.size(); i++) {
+//            System.out.print(a.get(i) + " ");
+//        }
+    }
 }
